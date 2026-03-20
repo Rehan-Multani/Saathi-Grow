4aa86656a519215d8f63f1b546de915cb70b7ffd
Muhammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2026-03-20 18:15:46 +0530

    env issue fix

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -1,10 +1,10 @@
 import admin from 'firebase-admin';
 // Firebase initialization using Environment Variables safely
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
   // Replace escaped \n with actual newlines in private key
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, ''),
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, ''),
 };
 
 admin.initializeApp({
